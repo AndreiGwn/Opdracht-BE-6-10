f31a5d8657c76376d5a16e8c3487fdaab41b3b13
Andrei Gogan <andrei.gogan1@gmail.com>
2026-06-27 19:07:29 +0200

    test: Add unit test for VoertuigInstructeur attributes assignment

---
+++ tests/Unit/VoertuigInstructeurTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\VoertuigInstructeur;
+
+class VoertuigInstructeurTest extends TestCase
+{
+    public function test_voertuig_instructeur_attributes_assignment(): void
+    {
+        $pivot = new VoertuigInstructeur([
+            'VoertuigId' => 1,
+            'InstructeurId' => 5,
+            'DatumToekenning' => '2017-06-18'
+        ]);
+
+        $this->assertEquals(1, $pivot->VoertuigId);
+        $this->assertEquals(5, $pivot->InstructeurId);
+        $this->assertEquals('2017-06-18', $pivot->DatumToekenning);
+    }
+}
